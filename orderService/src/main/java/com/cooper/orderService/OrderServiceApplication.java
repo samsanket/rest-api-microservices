d36cd77ebb7d01598c787b4d2c9b773ee51d0b0f
team-cores <sanket.deshpande@coreviwsystems.com>
2024-01-22 20:28:41 +0530

    Added basic eureka configuration

--- orderService/src/main/java/com/cooper/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/cooper/orderService/OrderServiceApplication.java
@@ -2,10 +2,13 @@ package com.cooper.orderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
